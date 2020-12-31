79001da1ff554486b26a48d3bf1c333859c4d6ee
Raz Elkayam <developer.raz@gmail.com>
2020-12-31 20:02:47 +0200

    adding functionality [WIP]

--- index.js
+++ index.js
@@ -18,25 +18,62 @@ app.use(bodyParser.json({ verify: rawBodyBuffer }));
 app.get('/health', (req, res) => {
     res.send('Server is running');
 });
+
+app.post('/test', (req, res) => {
+
+    console.dir("Testing endpoint:");
+    console.dir(req.body);
+    res.sendStatus(200);
+});
  
 app.post('/events', (req, res) => {
+
+    console.dir("Event from slack");
+    console.dir(req.body);
+    console.dir("---------")
  // App setting validation
     if (req.body.type === 'url_verification') {
         res.send(req.body.challenge);
     } else if (req.body.type === 'event_callback') { 
-        res.sendStatus(200);
-
+        res.sendStatus(200)
         const { bot_id, text, channel } = req.body.event;
         if (!text) return;
         let regex = /(^\/)/;
         if (bot_id || regex.test(text)) return;
         if (req.body.event.subtype === 'channel_join' || req.body.event.subtype === 'bot_add' || req.body.event.subtype === 'bot_remove') return;
-        sendRotationResult(channel, "Raz, Benji")
+
+        let stringArray = String(text.replace("rotate:", ""));
+
+        var arrays;
+
+        console.dir("string array: " + stringArray);
+
+        try {
+            arrays = JSON.parse(stringArray);
+          } catch (exception) {
+            console.dir(exception);
+            sendCantUndestandResult(channel, "Can't understand the intention. Please ask for a rotatoin with \"`rotate:[[\"name1\", \"name2\"], [\"name3\", \"name4\"] {...} ]`");
+            return;
+          }
+
+        var names = [];
+
+        arrays.forEach(array => { 
+            let random = Math.floor(Math.random() * array.length);
+            let name = array[random];
+            names.push(name);
+        }); 
+
+        sendRotationResult(channel, names.join(", "));
     }
 });
 
 const sendRotationResult = (channel, users) => {
-    axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text= Users for rotation are:' + users + '&pretty=1');
+    axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text= Users for rotation are :' + users + '&pretty=1');
+}
+
+const sendCantUndestandResult = (channel, message) => {
+    axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text=' + message + '&pretty=1');
 }
 
 const server = app.listen(process.env.PORT || 80, () => {
