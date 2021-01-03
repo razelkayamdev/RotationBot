5b149c28f554e32d51b4327f5ed17561fbe6604f
Raz Elkayam <developer.raz@gmail.com>
2021-01-03 14:19:56 +0200

    Finalizing functionality

--- index.js
+++ index.js
@@ -12,70 +12,94 @@ const rawBodyBuffer = (req, res, buf, encoding) => {
 };
 app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
 app.use(bodyParser.json({ verify: rawBodyBuffer }));
+
+const server = app.listen(process.env.PORT || 80, () => {
+    if (isDebug()) {
+        console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+    }
+});
  
-/* Handling events */
+/* Endpoints */
  
 app.get('/health', (req, res) => {
-    res.send('Server is running');
+    res.status(200).send('Server is running');
 });
 
 app.post('/test', (req, res) => {
-
-    console.dir("Testing endpoint:");
-    console.dir(req.body);
-    res.sendStatus(200);
+    if (isDebug()) { 
+        console.dir(req.body);
+        res.status(200).send(req.body);
+    } else {
+        res.sendStatus(200);
+    }
 });
  
+const rotateCommand = "rotate:"
 app.post('/events', (req, res) => {
+    if (isDebug()) {
+        console.dir("Event on /events:");
+        console.dir(req.body);
+    }
 
-    console.dir("Event from slack");
-    console.dir(req.body);
-    console.dir("---------")
- // App setting validation
     if (req.body.type === 'url_verification') {
+        /* Authentication challange from slack, as described here: https://api.slack.com/events/url_verification */
         res.send(req.body.challenge);
-    } else if (req.body.type === 'event_callback') { 
-        res.sendStatus(200)
+    } else if (req.body.type === 'event_callback') {
+        res.sendStatus(200);
+
+        if (req.body.event.subtype === 'channel_join' || req.body.event.subtype === 'bot_add' || req.body.event.subtype === 'bot_remove') return;
+        
         const { bot_id, text, channel } = req.body.event;
         if (!text) return;
         let regex = /(^\/)/;
         if (bot_id || regex.test(text)) return;
-        if (req.body.event.subtype === 'channel_join' || req.body.event.subtype === 'bot_add' || req.body.event.subtype === 'bot_remove') return;
 
-        let stringArray = String(text.replace("rotate:", ""));
-
-        var arrays;
-
-        console.dir("string array: " + stringArray);
-
-        try {
-            arrays = JSON.parse(stringArray);
-          } catch (exception) {
-            console.dir(exception);
-            sendCantUndestandResult(channel, "Can't understand the intention. Please ask for a rotatoin with \"`rotate:[[\"name1\", \"name2\"], [\"name3\", \"name4\"] {...} ]`");
-            return;
-          }
-
-        var names = [];
-
-        arrays.forEach(array => { 
-            let random = Math.floor(Math.random() * array.length);
-            let name = array[random];
-            names.push(name);
-        }); 
-
-        sendRotationResult(channel, names.join(", "));
+        if (text.includes(rotateCommand)) {
+            rotateParticipants(text, channel);
+        }
     }
 });
 
-const sendRotationResult = (channel, users) => {
-    axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text= Users for rotation are :' + users + '&pretty=1');
+const rotateParticipants = (text, channel) => {
+    let stringArray = String(text.replace(rotateCommand, ""));
+
+    var arrays;
+
+    try {
+        arrays = JSON.parse(fixJSON(stringArray));
+      } catch (exception) {
+        if (isDebug()) { 
+            console.dir(exception); 
+        }
+        sendCantUndestanRotationdResult(channel, unsuccesfulRotationMessage);
+        return;
+      }
+
+    var names = [];
+    arrays.forEach(array => { 
+        let random = Math.floor(Math.random() * array.length);
+        let name = array[random];
+        names.push(name);
+    }); 
+
+    sendRotationResult(channel, names.join(", "));
 }
 
-const sendCantUndestandResult = (channel, message) => {
+const isDebug = () => {
+    return (process.env.DEBUG === "true");
+}
+
+const fixJSON = (val) => {
+    return val.replace(/“/g, '"').replace(/”/g, '"');
+}
+
+const sendRotationResult = (channel, users) => {
+    axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text= ' + succesfulRotationMessage + ' `' + users + '` ' + '&pretty=1');
+}
+
+const sendCantUndestanRotationdResult = (channel, message) => {
     axios.post('https://slack.com/api/chat.postMessage?token=' + process.env.SLACK_ACCESS_TOKEN + '&channel=' + channel + '&text=' + message + '&pretty=1');
 }
 
-const server = app.listen(process.env.PORT || 80, () => {
-    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
+const succesfulRotationMessage = encodeURIComponent("Thank you for calling me and for showing interest in my capabilities!. I promise I didn’t let my personal preferences interfere with my decision 🤖 \nThe following persons have been selected for the rotation: 🥁🥁🥁...\n");
+const unsuccesfulRotationMessage = encodeURIComponent("I'm sorry 🤦, but  Can't understand your intention. Please use the following syntax for a rotation: `rotate:[[\"name1\", \"name2\"], [\"name3\", \"name4\"] {...} ]`");
